a1defba6353ead324705661c04d16e9d1a3fd90a
lmamak <lmamak@users.noreply.github.com>
2015-07-28 17:54:01 -0500

    Create 360link.js

---
+++ 360link.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$('.chat-button').replaceWith(
+'<h3 style="text-align: left;">Chat with a Librarian</h3>' +
+  '<iframe ' + 
+    'src="https://libraryh3lp.com/chat/ual_main_chat@chat.libraryh3lp.com?skin=23887&amp;theme=simpletext-flat&amp;identity=UA+Librarian" ' +
+    'frameborder="0" style="width: 99%; height: 200px; border: 0px solid #003366;">' +
+  '</iframe>' +
+  '<script type="text/javascript" src="https://libraryh3lp.com/js/libraryh3lp.js?multi"></script>'
+);
